75281faeaf216fe8fba87d1a2a4535cf6fd15a14
Microndgt <dgt_x@foxmail.com>
2017-09-01 23:51:31 +0800

    publish

---
+++ js/digit.js
@@ -0,0 +1,5 @@
+window.onload=function(){
+	document.getElementById('site.tags.length').innerHTML='26'
+	document.getElementById('site.posts.length').innerHTML='41'
+	document.getElementById('site.categories.length').innerHTML='2'
+}
