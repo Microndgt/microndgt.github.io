078d5b02487f49a58f5f157c9bf6c82bbf398208
Microndgt <dgt_x@foxmail.com>
2017-09-03 17:33:33 +0800

    publish

--- js/digit.js
+++ js/digit.js
@@ -1,5 +1,5 @@
 window.onload=function(){
 	document.getElementById('site.tags.length').innerHTML='27'
-	document.getElementById('site.posts.length').innerHTML='41'
+	document.getElementById('site.posts.length').innerHTML='42'
 	document.getElementById('site.categories.length').innerHTML='2'
 }
