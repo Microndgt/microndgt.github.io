278512f6c6fb9702568ca2e73d92913fc16289de
Microndgt <dgt_x@foxmail.com>
2017-10-19 12:36:05 +0800

    Site updated: 2017-10-19 12:36:04

---
+++ js/digit.js
@@ -0,0 +1,5 @@
+window.onload=function(){
+	document.getElementById('site.tags.length').innerHTML='34'
+	document.getElementById('site.posts.length').innerHTML='59'
+	document.getElementById('site.categories.length').innerHTML='3'
+}
