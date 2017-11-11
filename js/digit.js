64c615dfe235c4e535c2041558c5d665c7aa448e
Microndgt <dgt_x@foxmail.com>
2017-11-11 19:48:38 +0800

    publish

--- js/digit.js
+++ js/digit.js
@@ -1,5 +1,5 @@
 window.onload=function(){
-	document.getElementById('site.tags.length').innerHTML='39'
-	document.getElementById('site.posts.length').innerHTML='68'
+	document.getElementById('site.tags.length').innerHTML='40'
+	document.getElementById('site.posts.length').innerHTML='69'
 	document.getElementById('site.categories.length').innerHTML='4'
 }
