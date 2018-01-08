e27115dd59efc11c7e9e0373eca7f916c2ecc5c1
duguangting <dgt_x@foxmail.com>
2018-01-08 08:00:41 +0800

    publish

--- js/digit.js
+++ js/digit.js
@@ -1,5 +1,5 @@
 window.onload=function(){
 	document.getElementById('site.tags.length').innerHTML='46'
-	document.getElementById('site.posts.length').innerHTML='80'
+	document.getElementById('site.posts.length').innerHTML='81'
 	document.getElementById('site.categories.length').innerHTML='4'
 }
