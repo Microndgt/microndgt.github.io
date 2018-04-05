61c1bebb06f2663f14dae1a9199140ec2628ab11
duguangting <dgt_x@foxmail.com>
2018-04-05 14:38:05 +0800

    Site update 2018年 4月 5日 星期四 14时38分04秒 CST

--- js/digit.js
+++ js/digit.js
@@ -1,5 +1,5 @@
 window.onload=function(){
-	document.getElementById('site.tags.length').innerHTML='48'
-	document.getElementById('site.posts.length').innerHTML='89'
+	document.getElementById('site.tags.length').innerHTML='49'
+	document.getElementById('site.posts.length').innerHTML='90'
 	document.getElementById('site.categories.length').innerHTML='4'
 }
